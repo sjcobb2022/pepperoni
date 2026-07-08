81fbe1defc9efe65f404fc64d57dcd1748cf760f
Samuel Cobb <sjcobb2003@gmail.com>
2026-07-08 12:01:25 +0100

    note on synchronous PG stuff

--- src/pg.rs
+++ src/pg.rs
@@ -6,6 +6,11 @@ pub enum PgError {
     Command(String),
 }
 
+// TODO: Make these sync instead? Realistically I think that we want a fully sync connection with
+// postgres so that we can hard fail if any of our command fail. This means that we do not need to
+// wait on postgres if it fails. However handling postgres promotion for example can be done via
+// sqlx or a regular sql query, which is done asynchronously. Keep things async for the sake of a
+// clean api?
 pub trait PgCtl {
     async fn stop(&mut self) -> Result<(), PgError>;
     async fn promote(&mut self) -> Result<(), PgError>;
