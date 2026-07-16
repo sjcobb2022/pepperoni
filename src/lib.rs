5aca9e9759358a426a12bf96c83bf952bc2d367b
Samuel Cobb <sjcobb2003@gmail.com>
2026-07-16 01:07:58 +0100

    no_std lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// #![no_std]
+#![no_std]
 
 use core::time::Duration;
 
@@ -70,6 +70,7 @@ pub enum Event {
     ReleaseFailed,
 }
 
+#[derive(Clone, Copy)]
 pub enum Command {
     ObserveLease,
     TryAcquireLease { ttl: Duration },
@@ -81,139 +82,176 @@ pub enum Command {
     WakeAfter(Duration),
 }
 
-pub fn step(state: State, event: Event, cfg: &Config) -> (State, Vec<Command>) {
+#[derive(Clone, Copy)]
+/// A hardcoded structure of items. It is limited to two as there is at most 2 actions that can be
+/// followed at any one time. That being the immediate command evaluation, and a corresponding
+/// timeout for that action.
+pub struct Commands {
+    items: [Option<Command>; 2],
+}
+
+impl Commands {
+    /// Execute no commands.
+    fn none() -> Self {
+        Commands {
+            items: [None, None],
+        }
+    }
+    /// Execute one command.
+    fn one(a: Command) -> Self {
+        Commands {
+            items: [Some(a), None],
+        }
+    }
+    /// Execute 2 commands.
+    fn two(a: Command, b: Command) -> Self {
+        Commands {
+            items: [Some(a), Some(b)],
+        }
+    }
+}
+
+impl IntoIterator for Commands {
+    type Item = Command;
+    type IntoIter = core::iter::Flatten<core::array::IntoIter<Option<Command>, 2>>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.into_iter().flatten()
+    }
+}
+
+/// The main step function which contains the core logic for the library.
+pub fn step(state: State, event: Event, cfg: &Config) -> (State, Commands) {
     use State::*;
 
     match (state, event) {
         // Init
         // If we get triggered immediately, we should attempt to observe the current lease
-        (Init, Event::Timeout) => (ObservingFromInit, vec![Command::ObserveLease]),
+        (Init, Event::Timeout) => (ObservingFromInit, Commands::one(Command::ObserveLease)),
         // otherwise, we do nothing.
-        (s @ Init, _) => (s, vec![]),
+        (s @ Init, _) => (s, Commands::none()),
 
         // ObservingFromInit
         // If we are observing, and we are the leader, then we should stop since that is a failure
         (ObservingFromInit, Event::ObservedLeader(id)) if id == cfg.id => {
-            (Demoting, vec![Command::StopPg])
+            (Demoting, Commands::one(Command::StopPg))
         }
         // otherwise, we move into standby with the leader.
-        (ObservingFromInit, Event::ObservedLeader(id)) => {
-            (StartingStandby { leader: id }, vec![Command::StartStandby])
-        }
+        (ObservingFromInit, Event::ObservedLeader(id)) => (
+            StartingStandby { leader: id },
+            Commands::one(Command::StartStandby),
+        ),
         // If there is no leader, or our observation failed, we should try and acquire a lease.
         // If we do not have a connection to etcd or the etcd cluster as a whole, then then try
         // acquire will most likely fail.
         (ObservingFromInit, Event::ObservedNoLeader)
         | (ObservingFromInit, Event::ObserveFailed) => (
             AcquiringLease,
-            vec![
+            Commands::two(
                 Command::TryAcquireLease { ttl: cfg.lease_ttl },
-                Command::WakeAfter(cfg.lease_ttl), // if we have not acquired the lease by this time
-                                                   // then we will exit out of the acquiring stage
-            ],
+                Command::WakeAfter(cfg.lease_ttl),
+            ),
         ),
-        (s @ ObservingFromInit, _) => (s, vec![]),
+        (s @ ObservingFromInit, _) => (s, Commands::none()),
 
         // ObservingFromStandby
-        (ObservingFromStandby { .. }, Event::ObservedLeader(leader)) => {
-            (
-                Standby { leader },
-                vec![
-                    Command::WakeAfter(cfg.timeout), // we should wait until the timeout.
-                ],
-            )
-        }
+        (ObservingFromStandby { .. }, Event::ObservedLeader(leader)) => (
+            Standby { leader },
+            Commands::one(Command::WakeAfter(cfg.timeout)),
+        ),
 
         (ObservingFromStandby { .. }, Event::ObservedNoLeader) => (
             AcquiringLease,
-            vec![
+            Commands::two(
                 Command::TryAcquireLease { ttl: cfg.lease_ttl },
-                Command::WakeAfter(cfg.lease_ttl), // TODO: Change this
-            ],
+                Command::WakeAfter(cfg.lease_ttl),
+            ),
         ),
         (ObservingFromStandby { .. }, Event::ObserveFailed) => {
-            (Init, vec![Command::WakeAfter(Duration::ZERO)])
+            (Init, Commands::one(Command::WakeAfter(Duration::ZERO)))
         }
-        (s @ ObservingFromStandby { .. }, _) => (s, vec![]),
+        (s @ ObservingFromStandby { .. }, _) => (s, Commands::none()),
 
         // AcquiringLease
-        (AcquiringLease, Event::Timeout) => (Init, vec![Command::WakeAfter(Duration::ZERO)]),
+        (AcquiringLease, Event::Timeout) => {
+            (Init, Commands::one(Command::WakeAfter(Duration::ZERO)))
+        }
         (AcquiringLease, Event::AcquireSucceeded { term, ttl }) => (
             Promoting { term, ttl },
-            vec![Command::Promote { term }, Command::WakeAfter(ttl)],
+            Commands::two(Command::Promote { term }, Command::WakeAfter(ttl)),
         ),
-        (AcquiringLease, Event::AcquireFailed) => (
-            Init,
-            vec![
-                Command::WakeAfter(Duration::ZERO), // TODO: maybe backoff as to not throttle massively?
-            ],
-        ),
-        (s @ AcquiringLease, _) => (s, vec![]),
+        (AcquiringLease, Event::AcquireFailed) => {
+            (Init, Commands::one(Command::WakeAfter(Duration::ZERO)))
+        }
+        (s @ AcquiringLease, _) => (s, Commands::none()),
 
         // Promoting
-        (Promoting { .. }, Event::Timeout) => (Demoting, vec![Command::StopPg]),
+        (Promoting { .. }, Event::Timeout) => (Demoting, Commands::one(Command::StopPg)),
         (Promoting { term, ttl }, Event::PromoteSucceeded { term: t }) if t == term => (
             Leader { term },
-            vec![Command::WakeAfter(ttl.saturating_sub(cfg.renew_margin))],
+            Commands::one(Command::WakeAfter(ttl.saturating_sub(cfg.renew_margin))),
         ),
         (Promoting { term, .. }, Event::PromoteFailed { term: t }) if t == term => {
-            (Demoting, vec![Command::StopPg])
+            (Demoting, Commands::one(Command::StopPg))
         }
-        (s @ Promoting { .. }, _) => (s, vec![]),
+        (s @ Promoting { .. }, _) => (s, Commands::none()),
 
         // Leader
         (Leader { term }, Event::Timeout) => (
             Renewing { term },
-            vec![
+            Commands::two(
                 Command::RenewLease {
                     term,
                     ttl: cfg.lease_ttl,
                 },
                 Command::WakeAfter(cfg.renew_margin),
-            ],
+            ),
         ),
-        (s @ Leader { .. }, _) => (s, vec![]),
+        (s @ Leader { .. }, _) => (s, Commands::none()),
 
         // Renewing
-        (Renewing { .. }, Event::Timeout) => (Demoting, vec![Command::StopPg]),
+        (Renewing { .. }, Event::Timeout) => (Demoting, Commands::one(Command::StopPg)),
         (Renewing { term }, Event::RenewSucceeded { term: t, ttl }) if t == term => (
             Leader { term },
-            vec![Command::WakeAfter(ttl.saturating_sub(cfg.renew_margin))],
+            Commands::one(Command::WakeAfter(ttl.saturating_sub(cfg.renew_margin))),
         ),
         (Renewing { term, .. }, Event::RenewFailed { term: t }) if t == term => {
-            (Demoting, vec![Command::StopPg])
+            (Demoting, Commands::one(Command::StopPg))
         }
-        (s @ Renewing { .. }, _) => (s, vec![]),
+        (s @ Renewing { .. }, _) => (s, Commands::none()),
 
         // StartingStandby
-        (StartingStandby { leader }, Event::StandbyStarted) => {
-            (Standby { leader }, vec![Command::WakeAfter(cfg.timeout)])
-        }
+        (StartingStandby { leader }, Event::StandbyStarted) => (
+            Standby { leader },
+            Commands::one(Command::WakeAfter(cfg.timeout)),
+        ),
         (StartingStandby { .. }, Event::StandbyStartFailed) => {
-            (Init, vec![Command::WakeAfter(Duration::ZERO)])
+            (Init, Commands::one(Command::WakeAfter(Duration::ZERO)))
         }
-        (s @ StartingStandby { .. }, _) => (s, vec![]),
+        (s @ StartingStandby { .. }, _) => (s, Commands::none()),
 
         // Standby
         (Standby { leader }, Event::Timeout) if leader == cfg.id => {
-            (Demoting, vec![Command::StopPg])
+            (Demoting, Commands::one(Command::StopPg))
         }
-        (Standby { leader }, Event::Timeout) => {
-            (ObservingFromStandby { leader }, vec![Command::ObserveLease])
-        }
-        (s @ Standby { .. }, _) => (s, vec![]),
+        (Standby { leader }, Event::Timeout) => (
+            ObservingFromStandby { leader },
+            Commands::one(Command::ObserveLease),
+        ),
+        (s @ Standby { .. }, _) => (s, Commands::none()),
 
         // Demoting
-        (Demoting, Event::Stopped) => (ReleasingLease, vec![Command::ReleaseLease]),
-        (Demoting, Event::StopFailed) => (Stuck, vec![]),
-        (s @ Demoting, _) => (s, vec![]),
+        (Demoting, Event::Stopped) => (ReleasingLease, Commands::one(Command::ReleaseLease)),
+        (Demoting, Event::StopFailed) => (Stuck, Commands::none()),
+        (s @ Demoting, _) => (s, Commands::none()),
 
         // ReleasingLease
-        (ReleasingLease, Event::Released) => (Init, vec![Command::WakeAfter(Duration::ZERO)]),
-        (ReleasingLease, Event::ReleaseFailed) => (Stuck, vec![]),
-        (s @ ReleasingLease, _) => (s, vec![]),
+        (ReleasingLease, Event::Released) => {
+            (Init, Commands::one(Command::WakeAfter(Duration::ZERO)))
+        }
+        (ReleasingLease, Event::ReleaseFailed) => (Stuck, Commands::none()),
+        (s @ ReleasingLease, _) => (s, Commands::none()),
 
         // Stuck
-        (s @ Stuck, _) => (s, vec![]),
+        (s @ Stuck, _) => (s, Commands::none()),
     }
 }
